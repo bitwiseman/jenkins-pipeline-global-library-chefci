0628329dc7dce4f323ddf91395ba62e53a0161de
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-14 14:38:54 +0100

    Simpify catch

--- src/org/typo3/chefci/v2/stages/Acceptance.groovy
+++ src/org/typo3/chefci/v2/stages/Acceptance.groovy
@@ -25,7 +25,7 @@ class Acceptance extends AbstractStage {
                 script.withEnv(['KITCHEN_LOCAL_YAML=.kitchen.docker.yml']) {
                     try {
                         script.sh script: 'kitchen test --destroy always'
-                    } catch (Exception e) {
+                    } catch (err) {
                         script.echo "Archiving test-kitchen logs due to failure condition"
                         // archive includes: ".kitchen/logs/${instanceName}.log"
                         script.error "kitchen returned non-zero exit status"
